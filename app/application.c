d0a7a2ad75d7f812b55c0d36690fad9c2b7b0543
hubmartin <hub.martin@gmail.com>
2018-05-04 15:44:40 +0200

    Code cleanup

--- app/application.c
+++ app/application.c
@@ -1,9 +1,9 @@
 #include <application.h>
 
 #define BATTERY_VOLTAGE_DATA_STREAM_SAMPLES  6
+#define BATTERY_MODULE_UPDATE_INTERVAL_SECONDS (LORA_REPORT_INTERVAL_SECONDS / BATTERY_VOLTAGE_DATA_STREAM_SAMPLES)
 #define LORA_FIRST_REPORT_SECONDS (10)
 #define LORA_REPORT_INTERVAL_SECONDS (15 * 60)
-#define BATTERY_MODULE_UPDATE_INTERVAL_SECONDS (LORA_REPORT_INTERVAL_SECONDS / BATTERY_VOLTAGE_DATA_STREAM_SAMPLES)
 
 #define HEADER_EVENT_ERROR 0xff
 #define HEADER_EVENT_UPDATE 0x00
@@ -15,8 +15,8 @@ bc_led_t led;
 bc_cmwx1zzabz_t lora;
 bc_button_t button;
 uint8_t header = HEADER_EVENT_UPDATE;
-unsigned int channel_a_overflow_count = 0;
-unsigned int channel_b_overflow_count = 0;
+int channel_a_overflow_count = 0;
+int channel_b_overflow_count = 0;
 
 void pulse_counter_event_handler(bc_module_sensor_channel_t channel, bc_pulse_counter_event_t event, void *event_param)
 {
@@ -78,29 +78,29 @@ void lora_event_handler(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_event_t event, void
 
 void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
 {
-	(void) self;
-	(void) event_param;
+    (void) self;
+    (void) event_param;
 
-	if (event == BC_BUTTON_EVENT_HOLD)
-	{
-		bc_scheduler_plan_now(0);
-	}
+    if (event == BC_BUTTON_EVENT_HOLD)
+    {
+        bc_scheduler_plan_now(0);
+    }
 }
 
 void application_init(void)
 {
     bc_data_stream_init(&stream_battery_voltage_mv, 1, &stream_buffer_battery_voltage_mv);
 
-	bc_led_init(&led, BC_GPIO_LED, false, false);
-	bc_led_set_mode(&led, BC_LED_MODE_FLASH);
+    bc_led_init(&led, BC_GPIO_LED, false, false);
+    bc_led_set_mode(&led, BC_LED_MODE_FLASH);
 
-	bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_A, BC_PULSE_COUNTER_EDGE_FALL);
-	bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_A, pulse_counter_event_handler, NULL);
-	bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_B, BC_PULSE_COUNTER_EDGE_FALL);
+    bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_A, BC_PULSE_COUNTER_EDGE_FALL);
+    bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_A, pulse_counter_event_handler, NULL);
+    bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_B, BC_PULSE_COUNTER_EDGE_FALL);
     bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_B, pulse_counter_event_handler, NULL);
 
-	bc_module_battery_init(BC_MODULE_BATTERY_FORMAT_MINI);
-	bc_module_battery_set_update_interval(BATTERY_MODULE_UPDATE_INTERVAL_SECONDS * 1000);
+    bc_module_battery_init(BC_MODULE_BATTERY_FORMAT_MINI);
+    bc_module_battery_set_update_interval(BATTERY_MODULE_UPDATE_INTERVAL_SECONDS * 1000);
     bc_module_battery_set_event_handler(battery_module_event_handler, NULL);
 
     // Initialize LoRa
@@ -118,35 +118,35 @@ void application_init(void)
     bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
     bc_button_set_event_handler(&button, button_event_handler, NULL);
 
-	bc_scheduler_plan_absolute(0, LORA_FIRST_REPORT_SECONDS * 1000);
+    bc_scheduler_plan_absolute(0, LORA_FIRST_REPORT_SECONDS * 1000);
 }
 
 void application_task(void *param)
 {
-	(void) param;
+    (void) param;
 
-	if (!bc_cmwx1zzabz_is_ready(&lora))
-	{
-		bc_scheduler_plan_current_now();
-		return;
-	}
+    if (!bc_cmwx1zzabz_is_ready(&lora))
+    {
+        bc_scheduler_plan_current_now();
+        return;
+    }
 
     uint8_t buffer[12];
     uint32_t channel_count_a;
     uint32_t channel_count_b;
     int battery_voltage;
-	uint16_t battery_voltage_mv;
+    uint16_t battery_voltage_mv;
 
     channel_count_a = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_A);
     channel_count_b = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_B);
-	bc_data_stream_get_average(&stream_battery_voltage_mv, &battery_voltage);
+    bc_data_stream_get_average(&stream_battery_voltage_mv, &battery_voltage);
 
-	if ((channel_a_overflow_count > 0xff) || (channel_b_overflow_count > 0xff))
-	{
-	    header = HEADER_EVENT_ERROR;
-	}
+    if ((channel_a_overflow_count > 0xff) || (channel_b_overflow_count > 0xff))
+    {
+        header = HEADER_EVENT_ERROR;
+    }
 
-	battery_voltage_mv = (uint16_t)battery_voltage;
+    battery_voltage_mv = (uint16_t)battery_voltage;
 
     buffer[0] = header;
     buffer[1] = (((channel_a_overflow_count << 4) & 0xf0) | ((channel_b_overflow_count) & 0x0f));
@@ -161,12 +161,12 @@ void application_task(void *param)
     buffer[10] = battery_voltage_mv;
     buffer[11] = battery_voltage_mv >> 8;
 
-	if (bc_cmwx1zzabz_send_message(&lora, &buffer, sizeof(buffer)))
-	{
-		bc_scheduler_plan_current_relative(LORA_REPORT_INTERVAL_SECONDS * 1000);
-	}
-	else
-	{
-		bc_scheduler_plan_current_relative(1000);
-	}
+    if (bc_cmwx1zzabz_send_message(&lora, &buffer, sizeof(buffer)))
+    {
+        bc_scheduler_plan_current_relative(LORA_REPORT_INTERVAL_SECONDS * 1000);
+    }
+    else
+    {
+        bc_scheduler_plan_current_relative(1000);
+    }
 }
