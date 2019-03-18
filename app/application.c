bc70fec4296ef3e1b41459a5f86b6b016b508d22
Karel Blavka <karel.blavka@bigclown.com>
2019-03-18 11:02:44 +0100

    Update application

--- app/application.c
+++ app/application.c
@@ -1,20 +1,41 @@
 #include <application.h>
+#include <at.h>
 
-#define BATTERY_VOLTAGE_DATA_STREAM_SAMPLES  6
-#define BATTERY_MODULE_UPDATE_INTERVAL_SECONDS (LORA_REPORT_INTERVAL_SECONDS / BATTERY_VOLTAGE_DATA_STREAM_SAMPLES)
-#define LORA_FIRST_REPORT_SECONDS (10)
-#define LORA_REPORT_INTERVAL_SECONDS (15 * 60)
+#define SEND_DATA_INTERVAL         (15 * 60 * 1000)
+#define FIRST_SEND_DATA            (10 * 1000)
+#define MEASURE_INTERVAL           (1 * 60 * 1000)
+#define APPLICATION_TASK_ID        0
 
-#define HEADER_EVENT_ERROR 0xff
-#define HEADER_EVENT_UPDATE 0x00
+BC_DATA_STREAM_FLOAT_BUFFER(sm_voltage_buffer, 8)
+BC_DATA_STREAM_FLOAT_BUFFER(sm_temperature_buffer, (SEND_DATA_INTERVAL / MEASURE_INTERVAL))
+BC_DATA_STREAM_INT_BUFFER(sm_orientation_buffer, 3)
 
-BC_DATA_STREAM_INT_BUFFER(stream_buffer_battery_voltage_mv, BATTERY_VOLTAGE_DATA_STREAM_SAMPLES)
-bc_data_stream_t stream_battery_voltage_mv;
+bc_data_stream_t sm_voltage;
+bc_data_stream_t sm_temperature;
+bc_data_stream_t sm_orientation;
 
+// LED instance
 bc_led_t led;
-bc_cmwx1zzabz_t lora;
+// Button instance
 bc_button_t button;
-uint8_t header = HEADER_EVENT_UPDATE;
+// Lora modem instance
+bc_cmwx1zzabz_t lora;
+// Thermometer instance
+bc_tmp112_t tmp112;
+// Accelerometer instance
+bc_lis2dh12_t lis2dh12;
+bc_dice_t dice;
+
+bc_scheduler_task_id_t battery_measure_task_id;
+
+enum {
+    HEADER_BOOT         = 0x00,
+    HEADER_UPDATE       = 0x01,
+    HEADER_BUTTON_CLICK = 0x02,
+    HEADER_BUTTON_HOLD  = 0x03,
+
+} header = HEADER_BOOT;
+
 int channel_a_overflow_count = 0;
 int channel_b_overflow_count = 0;
 
@@ -35,138 +56,280 @@ void pulse_counter_event_handler(bc_module_sensor_channel_t channel, bc_pulse_co
     }
 }
 
-void battery_module_event_handler(bc_module_battery_event_t event, void *event_param)
+void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
 {
-    (void) event_param;
-
-    if (event == BC_MODULE_BATTERY_EVENT_UPDATE)
+    if (event == BC_BUTTON_EVENT_CLICK)
     {
-        float battery_voltage;
-        int battery_voltage_mv;
+        header = HEADER_BUTTON_CLICK;
 
-        bc_module_battery_get_voltage(&battery_voltage);
-        battery_voltage_mv = battery_voltage * 1000;
+        bc_scheduler_plan_now(APPLICATION_TASK_ID);
+    }
+    else if (event == BC_BUTTON_EVENT_HOLD)
+    {
+        header = HEADER_BUTTON_HOLD;
 
-        bc_data_stream_feed(&stream_battery_voltage_mv, &battery_voltage_mv);
+        bc_scheduler_plan_now(APPLICATION_TASK_ID);
     }
 }
 
-void lora_event_handler(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_event_t event, void *event_param)
-{ 
-    (void) event_param;
+void tmp112_event_handler(bc_tmp112_t *self, bc_tmp112_event_t event, void *event_param)
+{
+    if (event == BC_TMP112_EVENT_UPDATE)
+    {
+        float value;
 
-    if (event == BC_CMWX1ZZABZ_EVENT_READY)
+        bc_tmp112_get_temperature_celsius(self, &value);
+
+        bc_data_stream_feed(&sm_temperature, &value);
+    }
+}
+
+void battery_event_handler(bc_module_battery_event_t event, void *event_param)
+{
+    if (event == BC_MODULE_BATTERY_EVENT_UPDATE)
+    {
+        float voltage = NAN;
+
+        bc_module_battery_get_voltage(&voltage);
+
+        bc_data_stream_feed(&sm_voltage, &voltage);
+    }
+}
+
+void lis2dh12_event_handler(bc_lis2dh12_t *self, bc_lis2dh12_event_t event, void *event_param)
+{
+    if (event == BC_LIS2DH12_EVENT_UPDATE)
+    {
+        bc_lis2dh12_result_g_t g;
+
+        if (bc_lis2dh12_get_result_g(self, &g))
+        {
+            bc_dice_feed_vectors(&dice, g.x_axis, g.y_axis, g.z_axis);
+
+            int orientation = (int) bc_dice_get_face(&dice);
+
+            bc_data_stream_feed(&sm_orientation, &orientation);
+        }
+    }
+}
+
+void lora_callback(bc_cmwx1zzabz_t *self, bc_cmwx1zzabz_event_t event, void *event_param)
+{
+    if (event == BC_CMWX1ZZABZ_EVENT_ERROR)
+    {
+        bc_led_set_mode(&led, BC_LED_MODE_BLINK_FAST);
+    }
+    else if (event == BC_CMWX1ZZABZ_EVENT_SEND_MESSAGE_START)
+    {
+        bc_led_set_mode(&led, BC_LED_MODE_ON);
+
+        bc_scheduler_plan_relative(battery_measure_task_id, 20);
+    }
+    else if (event == BC_CMWX1ZZABZ_EVENT_SEND_MESSAGE_DONE)
     {
         bc_led_set_mode(&led, BC_LED_MODE_OFF);
     }
-
-    if (event == BC_CMWX1ZZABZ_EVENT_ERROR)
+    else if (event == BC_CMWX1ZZABZ_EVENT_READY)
     {
-        bc_led_set_mode(&led, BC_LED_MODE_BLINK);
-    }
-
-    if (event == BC_CMWX1ZZABZ_EVENT_JOIN_ERROR)
-    {
-        bc_led_set_mode(&led, BC_LED_MODE_BLINK);
-    }
-
-    if (event == BC_CMWX1ZZABZ_EVENT_SEND_MESSAGE_START)
-    {
-        bc_led_pulse(&led, 100);
+        bc_led_set_mode(&led, BC_LED_MODE_OFF);
     }
 }
 
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+void battery_measure_task(void *param)
 {
-    (void) self;
-    (void) event_param;
-
-    if (event == BC_BUTTON_EVENT_HOLD)
+    if (!bc_module_battery_measure())
     {
-        bc_scheduler_plan_now(0);
+        bc_scheduler_plan_current_now();
     }
+}
+
+bool at_send(void)
+{
+    bc_scheduler_plan_now(APPLICATION_TASK_ID);
+
+    return true;
+}
+
+bool at_status(void)
+{
+    float value_avg = NAN;
+
+    static const struct {
+        bc_data_stream_t *stream;
+        const char *name;
+        int precision;
+    } values[] = {
+            {&sm_voltage, "Voltage", 1},
+            {&sm_temperature, "Temperature", 1}
+    };
+
+    for (size_t i = 0; i < sizeof(values) / sizeof(values[0]); i++)
+    {
+        value_avg = NAN;
+
+        if (bc_data_stream_get_average(values[i].stream, &value_avg))
+        {
+            bc_atci_printf("$STATUS: \"%s\",%.*f", values[i].name, values[i].precision, value_avg);
+        }
+        else
+        {
+            bc_atci_printf("$STATUS: \"%s\",", values[i].name);
+        }
+    }
+
+    int orientation;
+
+    if (bc_data_stream_get_median(&sm_orientation, &orientation))
+    {
+        bc_atci_printf("$STATUS: \"Orientation\",%d", orientation);
+    }
+    else
+    {
+        bc_atci_printf("$STATUS: \"Orientation\",", orientation);
+    }
+
+    uint32_t channel_count_a  = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_A);
+
+    bc_atci_printf("$STATUS: \"Channel A count\",%d", channel_count_a);
+
+    uint32_t channel_count_b = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_B);
+
+    bc_atci_printf("$STATUS: \"Channel B count\",%d", channel_count_b);
+
+    return true;
 }
 
 void application_init(void)
 {
-    bc_data_stream_init(&stream_battery_voltage_mv, 1, &stream_buffer_battery_voltage_mv);
+    bc_data_stream_init(&sm_voltage, 1, &sm_voltage_buffer);
+    bc_data_stream_init(&sm_temperature, 1, &sm_temperature_buffer);
+    bc_data_stream_init(&sm_orientation, 1, &sm_orientation_buffer);
 
+    // Initialize LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
-    bc_led_set_mode(&led, BC_LED_MODE_FLASH);
+    bc_led_set_mode(&led, BC_LED_MODE_ON);
 
+    // Initialize Button
+    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
+    bc_button_set_event_handler(&button, button_event_handler, NULL);
+
+    // Initialize Thermometer
+    bc_tmp112_init(&tmp112, BC_I2C_I2C0, 0x49);
+    bc_tmp112_set_event_handler(&tmp112, tmp112_event_handler, NULL);
+    bc_tmp112_set_update_interval(&tmp112, MEASURE_INTERVAL);
+
+    // Initialize Battery
+    bc_module_battery_init();
+    bc_module_battery_set_event_handler(battery_event_handler, NULL);
+    battery_measure_task_id = bc_scheduler_register(battery_measure_task, NULL, 2020);
+
+    // Initialize Accelerometer
+    bc_dice_init(&dice, BC_DICE_FACE_UNKNOWN);
+    bc_lis2dh12_init(&lis2dh12, BC_I2C_I2C0, 0x19);
+    bc_lis2dh12_set_resolution(&lis2dh12, BC_LIS2DH12_RESOLUTION_8BIT);
+    bc_lis2dh12_set_event_handler(&lis2dh12, lis2dh12_event_handler, NULL);
+    bc_lis2dh12_set_update_interval(&lis2dh12, MEASURE_INTERVAL);
+
+    // Initialize Pulse Counter
     bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_A, BC_PULSE_COUNTER_EDGE_FALL);
     bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_A, pulse_counter_event_handler, NULL);
     bc_pulse_counter_init(BC_MODULE_SENSOR_CHANNEL_B, BC_PULSE_COUNTER_EDGE_FALL);
     bc_pulse_counter_set_event_handler(BC_MODULE_SENSOR_CHANNEL_B, pulse_counter_event_handler, NULL);
 
-    bc_module_battery_init(BC_MODULE_BATTERY_FORMAT_MINI);
-    bc_module_battery_set_update_interval(BATTERY_MODULE_UPDATE_INTERVAL_SECONDS * 1000);
-    bc_module_battery_set_event_handler(battery_module_event_handler, NULL);
-
-    // Initialize LoRa
+    // Initialize lora module
     bc_cmwx1zzabz_init(&lora, BC_UART_UART1);
-    bc_cmwx1zzabz_set_event_handler(&lora, lora_event_handler, NULL);
-
-    bc_cmwx1zzabz_set_band(&lora, BC_CMWX1ZZABZ_CONFIG_BAND_EU868);
+    bc_cmwx1zzabz_set_event_handler(&lora, lora_callback, NULL);
+    bc_cmwx1zzabz_set_mode(&lora, BC_CMWX1ZZABZ_CONFIG_MODE_ABP);
     bc_cmwx1zzabz_set_class(&lora, BC_CMWX1ZZABZ_CONFIG_CLASS_A);
 
-    //bc_cmwx1zzabz_set_appkey(&lora, "4257FAD1D73C3B7D4C2EDBAE6EBDA740");
-    //bc_cmwx1zzabz_set_appskey(&lora, "4357FAD1D73C3B7D4C2EDBAE6EBDA740");
-    //bc_cmwx1zzabz_set_nwkskey(&lora, "4457FAD1D73C3B7D4C2EDBAE6EBDA740");
-    bc_cmwx1zzabz_set_mode(&lora, BC_CMWX1ZZABZ_CONFIG_MODE_ABP);
+    // Initialize AT command interface
+    at_init(&led, &lora);
+    static const bc_atci_command_t commands[] = {
+            AT_LORA_COMMANDS,
+            {"$SEND", at_send, NULL, NULL, NULL, "Immediately send packet"},
+            {"$STATUS", at_status, NULL, NULL, NULL, "Show status"},
+            AT_LED_COMMANDS,
+            BC_ATCI_COMMAND_CLAC,
+            BC_ATCI_COMMAND_HELP
+    };
+    bc_atci_init(commands, BC_ATCI_COMMANDS_LENGTH(commands));
 
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    bc_button_set_event_handler(&button, button_event_handler, NULL);
-
-    bc_scheduler_plan_absolute(0, LORA_FIRST_REPORT_SECONDS * 1000);
+    bc_scheduler_plan_absolute(APPLICATION_TASK_ID, FIRST_SEND_DATA);
 }
 
 void application_task(void *param)
 {
-    (void) param;
-
     if (!bc_cmwx1zzabz_is_ready(&lora))
     {
-        bc_scheduler_plan_current_now();
+        bc_scheduler_plan_current_relative(100);
+
         return;
     }
 
-    uint8_t buffer[12];
-    uint32_t channel_count_a;
-    uint32_t channel_count_b;
-    int battery_voltage;
-    uint16_t battery_voltage_mv;
+    static uint8_t buffer[13];
 
-    channel_count_a = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_A);
-    channel_count_b = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_B);
-    bc_data_stream_get_average(&stream_battery_voltage_mv, &battery_voltage);
-
-    if ((channel_a_overflow_count > 0xff) || (channel_b_overflow_count > 0xff))
-    {
-        header = HEADER_EVENT_ERROR;
-    }
-
-    battery_voltage_mv = (uint16_t)battery_voltage;
+    memset(buffer, 0xff, sizeof(buffer));
 
     buffer[0] = header;
-    buffer[1] = (((channel_a_overflow_count << 4) & 0xf0) | ((channel_b_overflow_count) & 0x0f));
-    buffer[2] = channel_count_a;
-    buffer[3] = channel_count_a >> 8;
-    buffer[4] = channel_count_a >> 16;
-    buffer[5] = channel_count_a >> 24;
-    buffer[6] = channel_count_b;
-    buffer[7] = channel_count_b >> 8;
-    buffer[8] = channel_count_b >> 16;
-    buffer[9] = channel_count_b >> 24;
-    buffer[10] = battery_voltage_mv;
-    buffer[11] = battery_voltage_mv >> 8;
 
-    if (bc_cmwx1zzabz_send_message(&lora, &buffer, sizeof(buffer)))
+    float voltage_avg = NAN;
+
+    bc_data_stream_get_average(&sm_voltage, &voltage_avg);
+
+    if (!isnan(voltage_avg))
     {
-        bc_scheduler_plan_current_relative(LORA_REPORT_INTERVAL_SECONDS * 1000);
+        buffer[1] = voltage_avg * 10.f;
     }
-    else
+
+    int orientation;
+
+    if (bc_data_stream_get_median(&sm_orientation, &orientation))
     {
-        bc_scheduler_plan_current_relative(1000);
+        buffer[2] = orientation;
     }
+
+    float temperature_avg = NAN;
+
+    bc_data_stream_get_average(&sm_temperature, &temperature_avg);
+
+    if (!isnan(temperature_avg))
+    {
+        int16_t temperature_i16 = (int16_t) (temperature_avg * 10.f);
+
+        buffer[3] = temperature_i16 >> 8;
+        buffer[4] = temperature_i16;
+    }
+
+    uint32_t channel_count_a  = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_A);
+    uint32_t channel_count_b = bc_pulse_counter_get(BC_MODULE_SENSOR_CHANNEL_B);
+
+    buffer[5] = channel_count_a >> 24;
+    buffer[6] = channel_count_a >> 16;
+    buffer[7] = channel_count_a >> 8;
+    buffer[8] = channel_count_a;
+
+    buffer[9] = channel_count_b >> 24;
+    buffer[10] = channel_count_b >> 16;
+    buffer[11] = channel_count_b >> 8;
+    buffer[12] = channel_count_b;
+
+    if (!bc_cmwx1zzabz_send_message(&lora, buffer, sizeof(buffer)))
+    {
+        bc_scheduler_plan_current_from_now(1000);
+
+        return;
+    }
+
+    static char tmp[sizeof(buffer) * 2 + 1];
+
+    for (size_t i = 0; i < sizeof(buffer); i++)
+    {
+        sprintf(tmp + i * 2, "%02x", buffer[i]);
+    }
+
+    bc_atci_printf("$SEND: %s", tmp);
+
+    header = HEADER_UPDATE;
+
+    bc_scheduler_plan_current_relative(SEND_DATA_INTERVAL);
 }
